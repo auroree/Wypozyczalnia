79047002a862709b1f3db2c964fcc6c1d3368d11
auroree <auro123reg@gmail.com>
2014-04-15 19:40:11 +0200

    Zmiana obslugi BD na LINQ to SQL

---
+++ Wypozyczalnia/Wypozyczalnia/Database/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wypozyczalnia.Database
+{
+    public static class Extensions
+    {
+        // Konwersja IEnumerable<T> na DataTable
+        public static DataTable ToDataTable<T>(this IEnumerable<T> enumberable)
+        {
+            DataTable table = new DataTable("Generated");
+
+            T first = enumberable.FirstOrDefault();
+            if (first == null)
+                return table;
+
+            PropertyInfo[] properties = first.GetType().GetProperties();
+            foreach (PropertyInfo pi in properties)
+                table.Columns.Add(pi.Name, pi.PropertyType);
+
+            foreach (T t in enumberable)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyInfo pi in properties)
+                    row[pi.Name] = t.GetType().InvokeMember(pi.Name, BindingFlags.GetProperty, null, t, null);
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+    }
+
+}
